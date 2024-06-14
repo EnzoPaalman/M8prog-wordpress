9a6f567248667f7fb60ecc893dc23faa333f35fa
EnzoPaalman <35348@ma-web.nl>
2024-06-14 12:45:17 +0200

    les4

--- themes/Enzosthema/index.php
+++ themes/Enzosthema/index.php
@@ -1,6 +1,7 @@
-<?php
 
+<?php
+get_header();
 echo '<pre style="background:#0f0; padding: 2rem; width:100%;">';
 print_r( 'dit is mijn index pagina' );
 echo '</pre>';
-die(__FILE__.':'.__LINE__);
+get_footer();
